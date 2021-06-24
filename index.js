420978bbc9ac0120efce8d992d90004daa30862e
Humberto Angelucci <angeluccirincon@gmail.com>
2021-06-24 20:57:06 +0200

    Lab Done

--- index.js
+++ index.js
@@ -19,9 +19,60 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+  .then((response) => {
+    // return Recipe.create({
+    //   "title": "Asian Glazed Chicken Thighs",
+    //   "level": "Amateur Chef",
+    //   "ingredients": [
+    //     "1/2 cup rice vinegar",
+    //     "5 tablespoons honey",
+    //     "1/3 cup soy sauce (such as Silver Swan®)",
+    //     "1/4 cup Asian (toasted) sesame oil",
+    //     "3 tablespoons Asian chili garlic sauce",
+    //     "3 tablespoons minced garlic",
+    //     "salt to taste",
+    //     "8 skinless, boneless chicken thighs"
+    //   ],
+    //   "cuisine": "Asian",
+    //   "dishType": "main_course",
+    //   "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    //   "duration": 40,
+    //   "creator": "Chef LePapu"
+    // })
+    // .then ((response) => {
+    //   console.log(response.title)
+    // })
+
+   
+    return Recipe.insertMany( data )
+  
   })
+
+  .then((result) => {
+    result.forEach((elem) => {
+      console.log(elem.title);
+    })
+   })
+
+   .then(() => {
+    return Recipe.findOneAndUpdate ( {title: 'Rigatoni alla Genovese'} , {duration: 100}, {new: true})
+      .then(() => {
+        console.log('success!!');
+      })
+   })
+
+   .then(() => {
+    return Recipe.deleteOne({ title: 'Carrot Cake'})
+    .then(() => {
+      console.log('success Carrot Cake remove!!');
+    })
+   })
+
+   .then(() => {
+    return mongoose.connection.close();
+   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
